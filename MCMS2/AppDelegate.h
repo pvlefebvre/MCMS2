381b37ef7a8add5a467e99f3fea43f65851c8777
Paul Lefebvre <pvl4j@virginia.edu>
2016-06-01 20:01:11 -0500

    Initial Commit

---
+++ MCMS2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCMS2
+//
+//  Created by Paul Lefebvre on 6/1/16.
+//  Copyright © 2016 Paul Lefebvre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
